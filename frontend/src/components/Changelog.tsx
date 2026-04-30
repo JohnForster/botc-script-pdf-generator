543e15d0a3f6ef137b872ad57fccb6228047e809
John Forster <john.forster@makers.tech>
2026-04-30 11:38:38 +0100

    Update changelog

--- frontend/src/components/Changelog.tsx
+++ frontend/src/components/Changelog.tsx
@@ -10,6 +10,7 @@ const CHANGELOG_ENTRIES: ChangelogEntry[] = [
     date: "2026-04-30",
     changes: [
       "Use official icons again (Thank you TPI!)",
+      "Add support for Ventriloquist, Knaves, and God of Ug Lorics",
       "Add Community Created Content badge to scripts",
     ],
   },
